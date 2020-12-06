133fe39ad021d1533a613de1c0ff88ce9e7ea0fd
Josiah Bradbury <herrozerro@gmail.com>
2020-12-06 10:22:07 -0600

    Day 6 complete

--- AdventOfCode2020/Days/Day06.cs
+++ AdventOfCode2020/Days/Day06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020
@@ -10,7 +11,8 @@ namespace AdventOfCode2020
         {
             Console.WriteLine("Day 6");
 
-
+            Part1();
+            Part2();
 
             Console.WriteLine("**************");
             Console.WriteLine(Environment.NewLine);
@@ -18,12 +20,75 @@ namespace AdventOfCode2020
 
         public static void Part1()
         {
+            var lines = Utilities.GetLinesFromFile("day6.txt");
 
+            var groups = new List<group>();
+
+            var forms = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (line == string.Empty)
+                {
+                    groups.Add(new group() { groupid = groups.Count(), FormAnswers = forms.ToList() }); ;
+                    forms.Clear();
+                }
+                else
+                {
+                    forms.Add(line);
+                }
+            }
+
+            Console.WriteLine(groups.Sum(x => x.GetScore()));
+        }
+
+        public class group
+        {
+            public int groupid { get; set; }
+            public List<string> FormAnswers { get; set; }
+            
+            public int GetScore()
+            {
+                return FormAnswers.SelectMany(x => x.Select(y => y)).ToList().Distinct().Count();
+            }
+
+            public int GetCommonScores()
+            {
+                var alpha = "abcdefghijklmnopqrstuvwxyz";
+
+                int count = 0;
+
+                foreach (var letter in alpha)
+                {
+                    count += FormAnswers.All(l => l.Contains(letter)) ? 1 : 0;
+                }
+
+                return count;
+            }
         }
 
         public static void Part2()
         {
+            var lines = Utilities.GetLinesFromFile("day6.txt");
 
+            var groups = new List<group>();
+
+            var forms = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (line == string.Empty)
+                {
+                    groups.Add(new group() { groupid = groups.Count(), FormAnswers = forms.ToList() }); ;
+                    forms.Clear();
+                }
+                else
+                {
+                    forms.Add(line);
+                }
+            }
+
+            Console.WriteLine(groups.Sum(x => x.GetCommonScores()));
         }
     }
 }
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -9,7 +9,31 @@ namespace AdventOfCode2020
             Console.WriteLine("Hello World!");
 
             //Day01.RunDay();
-            Day05.RunDay();
+            //Day02.RunDay();
+            //Day03.RunDay();
+            //Day04.RunDay();
+            //Day05.RunDay();
+            Day06.RunDay();
+            //Day07.RunDay();
+            //Day08.RunDay();
+            //Day09.RunDay();
+            //Day10.RunDay();
+            //Day11.RunDay();
+            //Day12.RunDay();
+            //Day13.RunDay();
+            //Day14.RunDay();
+            //Day15.RunDay();
+            //Day16.RunDay();
+            //Day17.RunDay();
+            //Day18.RunDay();
+            //Day19.RunDay();
+            //Day20.RunDay();
+            //Day21.RunDay();
+            //Day22.RunDay();
+            //Day23.RunDay();
+            //Day24.RunDay();
+            //Day25.RunDay();
+            
         }
     }
 }
