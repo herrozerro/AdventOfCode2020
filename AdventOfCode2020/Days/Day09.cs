9e6f9492884f83149b68d25d0445d549284c790c
Josiah Bradbury <herrozerro@gmail.com>
2020-12-09 09:20:23 -0600

    day 9 complete

--- AdventOfCode2020/Days/Day09.cs
+++ AdventOfCode2020/Days/Day09.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020
@@ -10,7 +11,8 @@ namespace AdventOfCode2020
         {
             Console.WriteLine("Day 9");
 
-
+            Part1();
+            Part2();
 
             Console.WriteLine("**************");
             Console.WriteLine(Environment.NewLine);
@@ -18,12 +20,60 @@ namespace AdventOfCode2020
 
         public static void Part1()
         {
+            var lines = Utilities.GetLinesFromFile("day9.txt").Select(x => long.Parse(x)).ToList();
 
+            var preamble = 25;
+
+            var index = 0;
+
+            long lastgood = 0;
+
+            foreach (var line in lines.Skip(preamble))
+            {
+                var prevInts = lines.GetRange(index, preamble);
+
+                foreach (var prevint in prevInts)
+                {
+                    if (prevInts.Where(x => x != prevint).Any(x => x + prevint == line))
+                    {
+                        lastgood = line;
+                        break;
+                    }
+                }
+
+                if (lastgood != line)
+                {
+                    Console.WriteLine(line);
+                    return;
+                }
+
+                index++;
+            }
         }
 
         public static void Part2()
         {
+            var lines = Utilities.GetLinesFromFile("day9.txt").Select(x => long.Parse(x)).Where(x => x < 69316178).ToList();
 
+            long target = 69316178;
+
+            var i = 1;
+            foreach (var line in lines)
+            {
+                
+                var start = line;
+                var contigious = new List<long>() { line };
+                foreach (var item in lines.Skip(i))
+                {
+                    contigious.Add(item);
+                    if (contigious.Sum() == target)
+                    {
+                        Console.WriteLine($"min: {contigious.Min()} max: {contigious.Max()}  sum: {contigious.Min() + contigious.Max()}");
+                        return;
+                    }
+                }
+                i++;
+            }
         }
     }
 }
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -15,8 +15,8 @@ namespace AdventOfCode2020
             //Day05.RunDay();
             //Day06.RunDay();
             //Day07.RunDay();
-            Day08.RunDay();
-            //Day09.RunDay();
+            //Day08.RunDay();
+            Day09.RunDay();
             //Day10.RunDay();
             //Day11.RunDay();
             //Day12.RunDay();
